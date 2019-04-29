f9a0f4e4d7665970432fba91080bdb30b2fd8bc7
harikrishna12334 <github1233@gmail.com>
2019-04-29 13:28:58 +0530

    awscli scripts

---
+++ AWSCLI_commands/scripts/AWSCLI_User_creation_pwd_delete/profile_creation_Bat_Scripts/profile_creation.sh
@@ -0,0 +1,1 @@
+aws iam create-user --user-name dev & aws iam create-login-profile --user-name dev --password dev@123 --password-reset-required & aws iam create-access-key --user-name dev
---
+++ AWSCLI_commands/scripts/deleteall_instance.bat
@@ -0,0 +1,8 @@
+aws ec2 terminate-instances 
+        --instance-ids 
+         $(
+          aws ec2 describe-instances 
+            | grep InstanceId 
+            | awk {'print $2'} 
+            | sed 's/[",]//g'
+          )
